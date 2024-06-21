ebc47d507bd4aa88e927aed1132d9148713f5e44
jordanoskidavid <jordanoski.david16@gmail.com>
2024-06-21 14:46:49 +0200

    Did the menu-items components

--- frontend/Angular-MyCal/src/app/app.module.ts
+++ frontend/Angular-MyCal/src/app/app.module.ts
@@ -8,7 +8,10 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
-
+import { MenuItemComponent } from './menu-item/menu-item.component';
+import { MenuItem1Component } from './menu-item-1/menu-item-1.component';
+import { MenuItem2Component } from './menu-item-2/menu-item-2.component';
+import {FormsModule} from "@angular/forms";
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,12 +19,16 @@ import { MenuComponent } from './menu/menu.component';
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    MenuItemComponent,
+    MenuItem1Component,
+    MenuItem2Component
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [
     provideClientHydration()
   ],
